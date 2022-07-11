c779141b6cdf7d80493d83b72dd8a62c61dbf2d0
agouy <contact@agouy.com>
2022-07-11 10:45:41 +0000

    #1 improvements + test generation

--- main.R
+++ main.R
@@ -7,6 +7,8 @@ library(ggplot2)
 library(uuid)
 library(svglite)
 library(tim)
+library(grDevices)
+library(RColorBrewer)
 
 getValues <- function(ctx){
   values <- list()
@@ -41,13 +43,17 @@ input.par <- list(
   dot.size     = ctx$op.value("dot.size", type = as.double, default = 0.5),
   error.type   = ctx$op.value("error.type", type = as.character, default = "Standard Deviation"),
   bar.width    = ctx$op.value("bar.width", type = as.double, default = 0.25),
+  dodge.width  = ctx$op.value("dodge.width", type = as.double, default = 1.1),
+  jitter.width = ctx$op.value("jitter.width", type = as.double, default = 0.05),
   xlab         = ctx$op.value("xlab", type = as.character, default = ""),
   ylab         = ctx$op.value("ylab", type = as.character, default = ""),
   title        = ctx$op.value("title", type = as.character, default = ""),
   subtitle     = ctx$op.value("subtitle", type = as.character, default = ""),
-  caption      = ctx$op.value("caption", type = as.character, default = "")
+  caption      = ctx$op.value("caption", type = as.character, default = ""),
+  theme        = ctx$op.value("theme", type = as.character, default = "light"),
+  color.palette= ctx$op.value("color.palette", type = as.character, default = "Set1"),
+  wrap.1d      = ctx$op.value("wrap.1d", type = as.logical, default = TRUE)
 )
-
 
 df <- getValues(ctx)
 
@@ -76,11 +82,12 @@ if(!ctx$hasXAxis) {
   df$.x <- ""
 } 
 
-theme_set(theme_light())
+th <- get(paste0("theme_", input.par$theme))
+theme_set(th())
 
 ### Core plot with x and y axes
 plt <- ggplot(df_agg, aes_string(x = ".x", y = "mn", fill = fill.col)) +
-  geom_bar(position = position_dodge(width = 0.9), stat = "identity") +
+  geom_bar(position = position_dodge(width = input.par$dodge.width), stat = "identity") +
   labs(
     title = input.par$title,
     subtitle = input.par$subtitle,
@@ -92,12 +99,20 @@ plt <- ggplot(df_agg, aes_string(x = ".x", y = "mn", fill = fill.col)) +
 
 ### Add jitter
 if(input.par$jitter) {
-  plt <- plt + geom_jitter(
-    data = df,
-    aes_string(x = ".x", y = ".y", fill = fill.col),
-    size = input.par$dot.size,
-    position = position_jitterdodge(jitter.width = 0.1, dodge.width = 0.9)
-  )
+  if(is.null(fill.col)) {
+    plt <- plt + geom_jitter(
+      data = df,
+      aes_string(x = ".x", y = ".y", fill = fill.col),
+      size = input.par$dot.size
+    )
+  } else {
+    plt <- plt + geom_jitter(
+      data = df,
+      aes_string(x = ".x", y = ".y", fill = fill.col),
+      size = input.par$dot.size,
+      position = position_jitterdodge(jitter.width = input.par$jitter.width, dodge.width = input.par$dodge.width)
+    )
+  }
 }
 
 ### Add SD bars
@@ -105,9 +120,17 @@ if(input.par$error.type == "Standard Deviation") {
   plt <- plt + geom_errorbar(
     aes(ymin = mn - stdv, ymax = mn + stdv),
     width = input.par$bar.width,
-    position = position_dodge(width = 0.9)
+    position = position_dodge(width = input.par$dodge.width)
   )
 }
+
+### Color palette
+n_groups <- length(unique(df_agg[[fill.col]]))
+pal <- suppressWarnings(
+  colorRampPalette(brewer.pal(name = input.par$color.palette, n = n_groups))(n_groups)
+)
+plt <- plt + scale_fill_manual(values = pal)
+
 
 ### Facets based on rows and columns
 cnames <- unlist(ctx$cnames)
@@ -115,13 +138,27 @@ if(ctx$cnames[[1]] == "") cnames <- "."
 rnames <- unlist(ctx$rnames)
 if(ctx$rnames[[1]] == "") rnames <- "."
 
-plt <- plt + facet_grid(
-  as.formula(paste(
-    paste(rnames, collapse = "+"),
-    "~",
-    paste(cnames, collapse = "+")
-  ))
-)
+rnames <- paste0("`", rnames, "`")
+cnames <- paste0("`", cnames, "`")
+
+if(any(c(rnames, cnames) %in% ".") & input.par$wrap.1d) {
+  plt <- plt + facet_wrap(
+    as.formula(paste(
+      "~",
+      paste(rnames, collapse = "+"),
+      "+",
+      paste(cnames, collapse = "+")
+    ))
+  )
+} else {
+  plt <- plt + facet_grid(
+    as.formula(paste(
+      paste(rnames, collapse = "+"),
+      "~",
+      paste(cnames, collapse = "+")
+    ))
+  )
+}
 
 if (mimetype == 'image/png'){
   tmp <- tempfile(fileext = ".png")
